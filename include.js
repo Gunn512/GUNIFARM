ea78709115e54e1210bdc1ca30c809f550e36b47
Gunn512 <trongnguyen.gunn.job@gmail.com>
2025-12-08 01:41:47 +0700

    Update include.js

--- include.js
+++ include.js
@@ -1,3 +1,40 @@
+// Hàm thên class active
+
+function highlightActiveMenu() {
+  // Lấy đường dẫn hiện tại
+  const currentUrl = window.location.href;
+  const menuLinks = document.querySelectorAll(".navbar-main a, .menu-list a");
+
+  if (menuLinks.length === 0) return;
+  menuLinks.forEach((link) => {
+    if (currentUrl.includes(link.href)) {
+      // Thêm class active
+      link.classList.add("active");
+
+      const parentDropdown = link.closest(".dropdown");
+      if (parentDropdown) {
+        const parentLink = parentDropdown.querySelector("a");
+        if (parentLink) {
+          parentLink.classList.add("active");
+        }
+      }
+    }
+  });
+
+  const pathName = window.location.pathname;
+  if (pathName.endsWith("/") || pathName.endsWith("index.html")) {
+    menuLinks.forEach((link) => {
+      if (
+        link.getAttribute("href") === "index.html" ||
+        link.getAttribute("href") === "./"
+      ) {
+        link.classList.add("active");
+      }
+    });
+  }
+}
+
+// Hàm tìm và thay thế
 async function includeHTML(selector, file) {
   try {
     const el = document.querySelector(selector);
@@ -5,6 +42,8 @@ async function includeHTML(selector, file) {
     const res = await fetch(file);
     const html = await res.text();
     el.innerHTML = html;
+
+    highlightActiveMenu();
 
     if (window.lucide) {
       lucide.createIcons();
